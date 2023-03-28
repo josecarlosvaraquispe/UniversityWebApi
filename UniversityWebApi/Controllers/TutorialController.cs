58a7274818f72d8c9c82c625d53c9d9959a703e8
Aldo Medina <u202018286@upc.edu.pe>
2023-03-27 23:18:53 -0500

    Controladores y Clases Tutorial, Category añadidos

---
+++ UniversityWebApi/Controllers/TutorialController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UniversityWebApi.Models;
+
+namespace UniversityWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TutorialController : ControllerBase
+    {
+        // GET: api/Tutorial
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Tutorial/5
+        [HttpGet("{id}", Name = "GetTutorial")]
+        public Tutorial GetTutorial(int id)
+        {
+            Tutorial tutorial = new Tutorial();
+            tutorial.Id = id;
+            tutorial.Title = "Tutorial " + id.ToString();
+            tutorial.Year = 2023;
+            tutorial.Description = "Description of tutorial " + id.ToString();
+            
+            return tutorial;
+        }
+
+        // POST: api/Tutorial
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Tutorial/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/Tutorial/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ UniversityWebApi/Models/Tutorial.cs
@@ -0,0 +1,9 @@
+﻿namespace UniversityWebApi.Models;
+
+public class Tutorial
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Year { get; set; }
+    public string Description { get; set; }
+}
